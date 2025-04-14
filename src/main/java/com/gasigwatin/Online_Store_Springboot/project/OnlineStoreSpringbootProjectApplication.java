cfabc31ab0eaa5dc57a7209a751e0a8b21ce5f31
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-04-14 13:00:46 +0200

    apr14

---
+++ src/main/java/com/gasigwatin/Online_Store_Springboot/project/OnlineStoreSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gasigwatin.Online_Store_Springboot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gasigwatin/Online_Store_Springboot/project/OnlineStoreSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasigwatin.Online_Store_Springboot.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
